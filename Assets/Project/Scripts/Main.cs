fefada1a99295f0828d785c78aa5683bf1f853d9
BonzaiBarman <bonzaibarman@gmail.com>
2019-09-24 22:04:07 -0600

    Sept 24

--- Assets/Project/Scripts/Main.cs
+++ Assets/Project/Scripts/Main.cs
@@ -5,10 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+	public GameObject Horse1;
+	public GameObject Horse2;
+	public GameObject Horse3;
+	public GameObject Horse4;
+	// Start is called before the first frame update
+	void Start()
     {
-        
+
+	    Horse1.GetComponent<Animator>().speed = 0.87f;
+	    Horse2.GetComponent<Animator>().Play("Horse_Snooze", 0, 0f);
+	    Horse2.GetComponent<Animator>().speed = 0.92f;
+	    Horse3.GetComponent<Animator>().Play("Horse_ScratchChew", -1, 0.5f);
+	    Horse2.GetComponent<Animator>().speed = 0.89f;
+	    Horse4.GetComponent<Animator>().Play("Horse_Paw", -1, 0.5f);
+	    Horse2.GetComponent<Animator>().speed = 0.9f;
+
     }
 
     // Update is called once per frame
